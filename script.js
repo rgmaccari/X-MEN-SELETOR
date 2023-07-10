8eda7d05df7e9da4f6821291220b0031d771cd99
Rafael Gustavo Maccari <r.maccari@edu.unipar.br>
2023-07-10 15:47:22 -0300

    responsividade js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const personagens = document.querySelectorAll('.personagem');
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
         
+        if(window.innerWidth < 450){
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        };
+
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
         personagem.classList.add('selecionado');
